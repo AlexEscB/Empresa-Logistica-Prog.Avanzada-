6af8a7fe28f9de033410ea3dbde086cc3ec6db83
AlexEscB <alejandroescobarbarrios@gmail.com>
2026-09-11 21:31:11 -0500

    feat:App conecta las vistas y el controlador

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
+import controladores.CreadorDeConductoresVehiculosRutas;
+import vistas.Principal;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        CreadorDeConductoresVehiculosRutas.inicializarDatos();
+        Scanner scanner = new Scanner(System.in);
+        Principal.seleccionarOpcion(scanner);
+        scanner.close();
     }
 }
